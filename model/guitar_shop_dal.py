dad7f9a05221eca019a8babdc7e253c6664384d6
gossip girl <155579334+MikaylahHarrison@users.noreply.github.com>
2025-11-18 11:04:12 -0500

    9.19

--- model/guitar_shop_dal.py
+++ model/guitar_shop_dal.py
@@ -45,12 +45,33 @@ def get_all_products_in_category(category):
         with get_connection() as conn:
             cursor = conn.cursor()
             cursor.execute('''
-            SELECT Product.code, Product.name, Product.price
-            FROM Product JOIN category ON Product.category_id = category.id
-                           WHERE category.name = ?
+             SELECT Product.code, Product.name, Product.price
+            FROM Product JOIN category ON Product.category_id = Category.category_id
+                           WHERE category.name = "Guitars"
                            ''', [category])
             rows = cursor.fetchall()
     except sqlite3.Error as e:
         print(f"Failed to fetch data from database: {e}")
     return rows
+
+def add_product(category, name, code, price):
+    try:
+        with get_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute(''' SELECT category_id FROM Category WHERE name = ?
+            ''', [category])
+            row = cursor.fetchone()
+            if row is None:
+                raise ValueError(f"Category {category} does not exist")
+
+            category_id = row[0]
+
+            cursor.execute('''
+            INSERT INTO Product (category_id, name, code, price)
+                           VALUES (?, ?, ?, ?)''', [category_id, name, code, price])
+            conn.commit()
+        except sqlite3.Error as e:
+            print(f"Failed to add product to database: {e}")
+        except ValueError as e:
+            print(e)
 
